7a8fb9b7f80deeead62b62597aeb8773437814b0
Timothy Meixell <misternu@gmail.com>
2016-01-14 14:03:52 -0600

    Change guess storage to use the session, add manual session clear

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -5,12 +5,22 @@ end
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
+  if session[:guesses]
+    @last_guess = session[:guesses].select { |guess| guess[0] == @question.id }.last
+  end
   erb :'questions/show'
 end
 
-post '/questions/:id' do
-  @question = Question.find(params[:id])
-  @answer = params[:answer]
-  @correct = @question.check_answer(@answer)
-  erb :'questions/show'
+post '/questions/:id/guess' do
+  question = Question.find(params[:id])
+  answer = params[:answer]
+  correct = question.check_answer(answer)
+  session[:guesses] ||= []
+  session[:guesses] += [[question.id, correct, answer]]
+  redirect "/questions/#{params[:id]}"
+end
+
+get '/clear_session' do
+  session.clear
+  redirect '/questions'
 end
