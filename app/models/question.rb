797440a5a4fc804d527550cde049274535574f75
Timothy Meixell <misternu@gmail.com>
2016-01-15 17:05:40 -0600

    Standardize seeding a bit

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,7 @@ class Question < ActiveRecord::Base
   has_many :answers
 
   def check_answer(answer)
-    puts self.answers.first.text.inspect
-    puts answer.inspect
-    puts self.answers.first.text.inspect == answer.inspect
-    self.answers.pluck('text').include?(answer)
+    stripped_answer = answer.gsub(/\r/, '')
+    self.answers.pluck('text').include?(stripped_answer)
   end
 end
